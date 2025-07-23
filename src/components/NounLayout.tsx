86cca70627173c4fb54251bc78e8c7a5462f92a4
Samuel Jackson <samuelsjackson7@gmail.com>
2025-07-23 16:03:01 +0200

    basic red, green functionality, but i would prefer if they dont move

--- src/components/NounLayout.tsx
+++ src/components/NounLayout.tsx
@@ -11,40 +11,62 @@ type NounLayoutProps = {
 };
 
 export default function NounLayout({ currentCard }: NounLayoutProps) {
-  const [articles, setArticles] = useState([
+  const baseArticles = [
     { article: "der", wrong: false, right: false },
     { article: "die", wrong: false, right: false },
     { article: "das", wrong: false, right: false },
-  ]);
-  // const articles = ["der", "die", "das"];
-
-  // const articles = [
-  //   { article: "der", wrong: false, right: false },
-  //   { article: "die", wrong: false, right: false },
-  //   { article: "das", wrong: false, right: false },
-  // ];
+  ];
+  const [articles, setArticles] = useState(baseArticles);
 
   const handleClick = (e: React.MouseEvent, item: any) => {
     e.stopPropagation();
     console.log(`Article clicked: ${item.article}`);
     // if the article does not match the currentCard's article, turn the card red
     if (currentCard && currentCard.article !== item.article) {
-      console.log(`Correct article: ${currentCard.article}`);
-      // set relevant article to have wrong = true
+      const newArticle = { article: item.article, wrong: true, right: false };
+      const newlist = articles.filter((i) => i.article !== item.article);
+      newlist.push(newArticle);
+      return setArticles(newlist);
+    } else {
+      const newArticle = { article: item.article, wrong: false, right: true };
+      const newlist = articles.filter((i) => i.article !== item.article);
+      console.log("newlsit", newlist);
+      newlist.push(newArticle);
+      return setArticles(newlist);
     }
   };
 
   return (
     <div className="NounLayout-articles">
-      {articles.map((item, index) => (
-        <button
-          key={index}
-          className="NounLayout-article"
-          onClick={(e) => handleClick(e, item)}
-        >
-          {item.article}
-        </button>
-      ))}
+      {articles.map((item, index) =>
+        item.right ? (
+          <button
+            key={index}
+            className="NounLayout-article"
+            onClick={(e) => handleClick(e, item)}
+            style={{ backgroundColor: "green" }}
+          >
+            {item.article}
+          </button>
+        ) : item.wrong ? (
+          <button
+            key={index}
+            className="NounLayout-article"
+            onClick={(e) => handleClick(e, item)}
+            style={{ backgroundColor: "red" }}
+          >
+            {item.article}
+          </button>
+        ) : (
+          <button
+            key={index}
+            className="NounLayout-article"
+            onClick={(e) => handleClick(e, item)}
+          >
+            {item.article}
+          </button>
+        )
+      )}
     </div>
   );
 }
